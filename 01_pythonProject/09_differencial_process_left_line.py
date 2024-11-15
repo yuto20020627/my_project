5848c27a6e7431dd88461507fcfde1e0a7292da1
yuto20020627 <yuto20020627@icloud.com>
2024-11-15 18:39:09 +0900

    06,07,変更 08,09,Queue_display_left,を追加

--- 01_pythonProject/09_differencial_process_left_line.py
+++ 01_pythonProject/09_differencial_process_left_line.py
@@ -1,4 +1,4 @@
-#動画からHTML表示、MOG2を使わない、元フレーム、前景マスクを表示
+#動画からHTML表示、MOG2を使わない、元フレーム、前景マスクを表示、一番左の人に縦線、画像をバスの待ち列
 import cv2
 import numpy as np  # numpy　列の数値計算
 from flask import Flask, render_template, jsonify  # render_template, jsonify の修正
@@ -8,7 +8,7 @@ import threading
 app = Flask(__name__)
 
 # 動画の読み込みまたはカメラを起動
-cap = cv2.VideoCapture('video/ScreenRecording_10-25-2024 15-45-20_1.mov')
+cap = cv2.VideoCapture('video/IMG_2420.MOV')
 
 # 最初のフレームを背景モデルとして取得
 ret, background = cap.read()  # ret　正常に動画を読み込めたか background 動画を格納
@@ -27,17 +27,19 @@ min_contour_area = 10 * 10  # 最小の検出面積（10x10ピクセル）
 # 検出したエリアを保持するための辞書
 detected_areas = {}  # キーとして(x,y,w,h)を持ち、値として何フレーム検出したかを持つx,yは左上の座標、wは幅、hは高さ
 
-# 最も右側のX座標を初期化
-max_right_x = 0
-
 # 動画出力の設定
 fourcc = cv2.VideoWriter_fourcc(*'XVID')  # 出力動画のフォーマット
 out = cv2.VideoWriter('output_with_line.avi', fourcc, 30.0, (int(cap.get(3)), int(cap.get(4))))
 
+#動画のフレームの幅(3)、高さ(4)を設定
+min_left_x = cap.get(3)
+height = cap.get(4)
+print('幅：' + str(min_left_x) + '高さ：' + str(height))
+
 # バス待ち時間ページの情報を定期的に更新
 def process_video():
 
-    global max_right_x
+    global min_left_x
     while cap.isOpened():  # 動画が開かれているか
         ret, frame = cap.read()  # 動画からフレームを1つ読み込む関数
         if not ret:  # ret == False
@@ -58,7 +60,7 @@ def process_video():
         # 輪郭抽出
         contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-        max_right_x_local = 0
+        min_left_x_local = cap.get(3) #フレーム幅を最小値
         # 各輪郭をループ処理
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)  # 最小の外接矩形を計算
@@ -77,17 +79,17 @@ def process_video():
                 if detected_areas[(x, y, w, h)] >= min_duration_frames:
                     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # 緑色の矩形を描画
 
-                # 右端のX座標を更新,縦線を引くため
-                right_x = x + w
-                if right_x > max_right_x_local:
-                    max_right_x_local = right_x
-        # max_right_xの値を更新
-        max_right_x = max_right_x_local
+                # 左端のX座標を更新,縦線を引くため
+                left_x = x
+                if left_x < min_left_x_local:
+                    min_left_x_local = left_x
+        # min_lift_xの値を更新
+        min_left_x = min_left_x_local
 
-        # 最も右側のX座標に基づいて縦線を描画
-        if max_right_x > 0:
-            cv2.line(frame, (max_right_x, 0), (max_right_x, frame.shape[0]), (255, 0, 0), 2)  # 元のフレームに青線を描画
-            cv2.line(fg_mask, (max_right_x, 0), (max_right_x, fg_mask.shape[0]), (255, 0, 0), 2)  # 前景マスクに青線を描画
+        # 最も左側のX座標に基づいて縦線を描画
+        if min_left_x < cap.get(3):
+            cv2.line(frame, (int(min_left_x), 0), (int(min_left_x), frame.shape[0]), (255, 0, 0), 2)  # 元のフレームに青線を描画
+            cv2.line(fg_mask, (int(min_left_x), 0), (int(min_left_x), fg_mask.shape[0]), (255, 0, 0), 2)  # 前景マスクに青線を描画
 
         # 動画として出力
         out.write(frame)  # フレームを動画として保存
@@ -108,12 +110,12 @@ def process_video():
 # バス待ち時間の情報をJSON形式で返す
 @app.route('/get_bus_time')
 def get_bus_time():
-    return jsonify({'line': max_right_x})
+    return jsonify({'line': min_left_x})
 
 # HTMLページをレンダリング
 @app.route('/')
 def index():
-    return render_template('Queue_display.html')
+    return render_template('Queue_display_left.html')
 
 # 動画処理を別スレッドで実行
 thread = threading.Thread(target=process_video)
