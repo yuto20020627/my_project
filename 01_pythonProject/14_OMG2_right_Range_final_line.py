ecc72d27fbde8bde6a5e3a2070def14d91c73a3f
yuto20020627 <yuto20020627@icloud.com>
2024-11-26 16:50:06 +0900

    2024-11-25 Windows

--- 01_pythonProject/14_OMG2_right_Range_final_line.py
+++ 01_pythonProject/14_OMG2_right_Range_final_line.py
@@ -1,7 +1,7 @@
-#MOG2を利用した背景差分法
-#11_differencialにモルフォロジー変換を追加、MOG2のパラメータ調整
-#画像認識の範囲を指定
-#final_lineはない
+# MOG2を利用した背景差分法
+# 11_differencialにモルフォロジー変換を追加、MOG2のパラメータ調整
+# 画像認識の範囲を指定
+#常に最後尾の線が描写されるように変更
 import cv2
 import numpy as np
 import threading
@@ -13,7 +13,7 @@ app = Flask(__name__)
 # 動画の読み込み
 cap = cv2.VideoCapture('video/IMG_2435_1.mov')
 
-#動画のフレームの幅、高さ、フレーム数、総フレーム数、再生時間を設定、出力
+# 動画のフレームの幅、高さ、フレーム数、総フレーム数、再生時間を設定、出力
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_FPS = int(cap.get(cv2.CAP_PROP_FPS))
@@ -23,15 +23,18 @@ print('幅：' + str(frame_width) + ' 高さ：' + str(frame_height))
 print('フレーム数:' + str(frame_FPS) + ' 総フレーム数:' + str(frame_FPS_count))
 print('動画の再生時間(秒):' + str(frame_FPS_count / frame_FPS))
 
-# 任意の多角形エリアを設定
+# 検出を行う多角形エリアを設定
 roi_area = np.array([[0, 616], [0, 570], [1042, 85], [1042, 616]])
 
 # グローバル変数（縦線の位置）
 max_right_x = 0
+frame_count = 0
+last_max_right_x = 0
 
 def process_video():
-
     global max_right_x
+    global frame_count
+    global last_max_right_x
 
     # 背景差分法のセットアップ、MOG2のセットアップ
     # history(500):背景モデルの更新回数　varThreshold(16):前景判断のしきい値　detectShadows(True):影の検出を有効にするか
@@ -53,7 +56,7 @@ def process_video():
         # 背景差分でフレームに対する前景を抽出、背景：黒（０）、前景：白（２５５）
         fg_mask = background_subtractor.apply(frame)
 
-        #モルフォロジー変換を用いたノイズ除去
+        # モルフォロジー変換を用いたノイズ除去
         fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
 
         # マスクを適用して、指定エリア外の部分を黒くする
@@ -68,12 +71,19 @@ def process_video():
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
             if h > w and h > 30 and w > 20:  # 条件に合う輪郭をフィルタリング
-                right_x = x + w #左端＋幅で右端の座標
-                if right_x > max_right_x_local:#１つのフレームで同時に2つ以上の人間を検出した際、最も右の人に線を引くため
+                right_x = x + w  # 左端＋幅で右端の座標
+                if right_x > max_right_x_local:  # １つのフレームで同時に2つ以上の人間を検出した際、最も右の人に線を引くため
                     max_right_x_local = right_x
         ################################################################
+        # 変更があれば今回のフレームでのmax_right_xの更新、なければ更新しない
+        if max_right_x_local > 0:
+            max_right_x = max_right_x_local  # 今回のフレームでのmax_right_xの更新
+            frame_count = 0 # カウントリセット
+        else:
+            frame_count = frame_count + 1
 
-        max_right_x = max_right_x_local#今回のフレームでのmax_right_xの更新
+        if frame_count >= 10:# and max_right_x==frame_width
+            last_max_right_x = max_right_x
 
         # マスク範囲を緑色で描画
         # cv2.polylines(画像名、[すべての頂点]、isClosed=始点と終点を結ぶか、color=（色）、thickness=線の太さ)
@@ -83,6 +93,8 @@ def process_video():
         if max_right_x > 0:
             # cv2.line(画像名、（始点）、（終点）、（色）、線の太さ)
             cv2.line(frame, (max_right_x, 0), (max_right_x, frame_height), (255, 0, 0), 2)
+            cv2.line(frame, (last_max_right_x, 0), (last_max_right_x, frame_height), (0, 0, 255), 2)
+
 
         # 結果の表示(出力時の名前、画像名)
         cv2.imshow('Queue_Video', frame)
@@ -95,15 +107,18 @@ def process_video():
     cap.release()
     cv2.destroyAllWindows()
 
+
 # 縦線の位置をJSON形式で返す
 @app.route('/get_bus_time')
 def get_bus_time():
     return jsonify({'line': max_right_x})
 
+
 # HTMLページをレンダリング
 @app.route('/')
 def index():
     return render_template('Queue_display_right.html')
+
 
 # 動画処理を別スレッドで実行
 thread = threading.Thread(target=process_video)
