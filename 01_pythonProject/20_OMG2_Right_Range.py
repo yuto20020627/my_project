bf6d1b77142bd8591c89bbdcb855bdd4d3dda9cc
yuto20020627 <yuto20020627@icloud.com>
2025-01-22 13:38:34 +0900

    2024-1-22 Windows

--- 01_pythonProject/20_OMG2_Right_Range.py
+++ 01_pythonProject/20_OMG2_Right_Range.py
@@ -28,14 +28,14 @@ roi_area = np.array([[0, 616], [0, 510], [135, 510], [1042, 85], [1042, 616]])
 # グローバル変数（縦線の位置）
 max_right_x = 0
 frame_count = 0
-last_max_right_x = 0
-position_history = []
+confirmed_line_x = 0
+history = []
 
 def process_video():
     global max_right_x
     global frame_count
-    global last_max_right_x
-    global position_history
+    global confirmed_line_x
+    global history
 
     # 背景差分法のセットアップ、MOG2のセットアップ
     # history(500):背景モデルの更新回数　varThreshold(16):前景判断のしきい値　detectShadows(True):影の検出を有効にするか
@@ -50,19 +50,14 @@ def process_video():
 
         # フレームと同じサイズの全黒マスクを作成（全体を黒にする）np.zeros すべての初期値0の配列
         mask = np.zeros((frame_height, frame_width), dtype=np.uint8)
-
         # マスクに多角形を描画（多角形内を白色で塗りつぶし）
         cv2.fillPoly(mask, [roi_area], 255)
-
         # 背景差分でフレームに対する前景を抽出、背景：黒（０）、前景：白（２５５）必要
         fg_mask = background_subtractor.apply(frame)
-
         # モルフォロジー変換を用いたノイズ除去
         fg_mask = cv2.morphologyEx(fg_mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
-
         # マスクを適用して、指定エリア外の部分を黒くする
         fg_mask = cv2.bitwise_and(fg_mask, mask)
-
         # 前景から輪郭の抽出　必要
         contours, _ = cv2.findContours(fg_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -81,18 +76,18 @@ def process_video():
             max_right_x = max_right_x_local# 今回のフレームでのmax_right_xの更新
         ######確定線の更新(2秒)#############################
         # 位置履歴に現在の位置を追加
-        position_history.append(max_right_x) #.appendで配列の末尾に追加
-        if len(position_history) > 60: # 履歴を60フレーム分に制限
-            position_history.pop(0)
+        history.append(max_right_x) #.appendで配列の末尾に追加
+        if len(history) > 60: # 履歴を60フレーム分に制限
+            history.pop(0)
 
         # 確定線の更新(過去60フレームの数値が誤差+-5なら更新)
-        if len(position_history) == 60 and max_right_x != frame_width:
+        if len(history) == 60 and max_right_x != frame_width:
             count = 0
             for i in range(1,60):
-                if abs(position_history[i] - position_history[0]) <= 5:
+                if abs(history[i] - history[0]) <= 5:
                     count = count + 1
             if count == 59:
-                last_max_right_x = max_right_x  # 赤線の位置を更新
+                confirmed_line_x = max_right_x  # 赤線の位置を更新
         ##################################################
         # マスク範囲を緑色で描画
         # cv2.polylines(画像名、[すべての頂点]、isClosed=始点と終点を結ぶか、color=（色）、thickness=線の太さ)
@@ -102,7 +97,7 @@ def process_video():
         if max_right_x > 0:
             # cv2.line(画像名、（始点）、（終点）、（色）、線の太さ)
             cv2.line(frame, (max_right_x, 0), (max_right_x, frame_height), (255, 0, 0), 2)
-            cv2.line(frame, (last_max_right_x, 0), (last_max_right_x, frame_height), (0, 0, 255), 2)
+            cv2.line(frame, (confirmed_line_x, 0), (confirmed_line_x, frame_height), (0, 0, 255), 2)
 
         # 結果の表示(出力時の名前、画像名)
         cv2.imshow('Queue_Video', frame)
@@ -111,7 +106,6 @@ def process_video():
         if cv2.waitKey(30) & 0xFF == 27:
             break
     ###################################################################################################
-
     cap.release()
     cv2.destroyAllWindows()
 
@@ -141,14 +135,11 @@ def get_next_bus_time(times):
 @app.route('/get_bus_time')
 def get_bus_time():
     bus_schedule = read_bus_schedule()
-    for bus_stop_times in bus_schedule["bus_stops"]:
-        next_bus_time = get_next_bus_time(bus_stop_times)
-        if next_bus_time:
-            break  # 最初に見つかった時刻でループを抜ける
+    next_bus_time = get_next_bus_time(bus_schedule["bus_stops"][0])
 
     return jsonify({
-        'line': last_max_right_x,
-        'next_buses': next_bus_times
+        'line': confirmed_line_x,
+        'next_buses': next_bus_time
     })
 
 # HTMLページをレンダリング
